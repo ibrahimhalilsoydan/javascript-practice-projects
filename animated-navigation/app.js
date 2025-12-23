864e8620e17b4f39e4743d6044594df9e5ffba19
ibrahim halil SOYDAN <ibrahimhalilsoydan@gmail.com>
2025-12-23 09:01:31 +0300

    animated-navigation added

---
+++ animated-navigation/app.js
@@ -0,0 +1,7 @@
+const navMenu = document.getElementById("nav");
+const toggleB =document.getElementById("toggle")
+
+
+toggleB.addEventListener("click",()=>{
+    navMenu.classList.toggle("active")
+})
